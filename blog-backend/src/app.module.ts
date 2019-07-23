65a713377a59f00aaf611ebe22bba413c7fa3fed
Jeffrey Wolff <jeff.wolff@spellingcity.com>
2019-07-23 09:04:44 -0400

    first commit

---
+++ blog-backend/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-blog', { useNewUrlParser: true }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
